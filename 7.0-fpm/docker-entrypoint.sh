84be081b86f2639b31b43a94c3561de95570668c
Oleh Posyniak <oposyniak@magento.com>
2018-03-06 10:22:42 -0600

    Improve history

--- 7.0-fpm/docker-entrypoint.sh
+++ 7.0-fpm/docker-entrypoint.sh
@@ -2,36 +2,9 @@
 
 [ "$DEBUG" = "true" ] && set -x
 
-# If asked, we'll ensure that the www-data is set to the same uid/gid as the
-# mounted volume.  This works around permission issues with virtualbox shared
-# folders.
-if [[ "$UPDATE_UID_GID" = "true" ]]; then
-    echo "Updating www-data uid and gid"
-
-    DOCKER_UID=`stat -c "%u" $MAGENTO_ROOT`
-    DOCKER_GID=`stat -c "%g" $MAGENTO_ROOT`
-
-    INCUMBENT_USER=`getent passwd $DOCKER_UID | cut -d: -f1`
-    INCUMBENT_GROUP=`getent group $DOCKER_GID | cut -d: -f1`
-
-    echo "Docker: uid = $DOCKER_UID, gid = $DOCKER_GID"
-    echo "Incumbent: user = $INCUMBENT_USER, group = $INCUMBENT_GROUP"
-
-    # Once we've established the ids and incumbent ids then we need to free them
-    # up (if necessary) and then make the change to www-data.
-
-    [ ! -z "${INCUMBENT_USER}" ] && usermod -u 99$DOCKER_UID $INCUMBENT_USER
-    usermod -u $DOCKER_UID www-data
-
-    [ ! -z "${INCUMBENT_GROUP}" ] && groupmod -g 99$DOCKER_GID $INCUMBENT_GROUP
-    groupmod -g $DOCKER_GID www-data
-fi
-
 # Ensure our Magento directory exists
 mkdir -p $MAGENTO_ROOT
 chown www-data:www-data $MAGENTO_ROOT
-
-
 
 # Configure Sendmail if required
 if [ "$ENABLE_SENDMAIL" == "true" ]; then
@@ -46,10 +19,8 @@ fi
     docker-php-ext-enable xdebug && \
     echo "Xdebug is enabled"
 
-
 # Configure PHP-FPM
 [ ! -z "${MAGENTO_RUN_MODE}" ] && sed -i "s/!MAGENTO_RUN_MODE!/${MAGENTO_RUN_MODE}/" /usr/local/etc/php-fpm.conf
-
 
 exec "$@"
 
