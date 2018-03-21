be6c088978b56da600ec9feb0adaa45ddc4ff8a9
Oleh Posyniak <oposyniak@magento.com>
2018-03-21 14:24:56 -0500

    Merge changes

--- 7.1-cli/docker-entrypoint.sh
+++ 7.1-cli/docker-entrypoint.sh
@@ -19,4 +19,13 @@ fi
     docker-php-ext-enable xdebug && \
     echo "Xdebug is enabled"
 
+# Configure composer
+[ ! -z "${COMPOSER_GITHUB_TOKEN}" ] && \
+    composer config --global github-oauth.github.com $COMPOSER_GITHUB_TOKEN
+
+[ ! -z "${COMPOSER_MAGENTO_USERNAME}" ] && \
+    composer config --global http-basic.repo.magento.com \
+        $COMPOSER_MAGENTO_USERNAME $COMPOSER_MAGENTO_PASSWORD
+
 exec "$@"
+
