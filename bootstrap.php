12c9f3a0885ba4f54b28ed54dc8b484ec2bba1b9
Oleh Posyniak <oposyniak@magento.com>
2018-03-29 15:09:21 -0500

    Added a CLI command to generate PHP configs

---
+++ bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+define('BP', __DIR__);
+define('DATA', __DIR__ . '/data');
+
+return require __DIR__ . '/vendor/autoload.php';
---
+++ src/Command/Generate/Php.php
@@ -0,0 +1,97 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Mcd\Command\Generate;
+
+use Illuminate\Contracts\Filesystem\FileNotFoundException;
+use Illuminate\Filesystem\Filesystem;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * @inheritdoc
+ */
+class Php extends Command
+{
+    const SUPPORTED_VERSIONS = ['7.0', '7.1', '7.2'];
+    const EDITIONS = ['cli', 'fpm'];
+
+    const ARGUMENT_VERSION = 'version';
+
+    /**
+     * @var Filesystem
+     */
+    private $filesystem;
+
+    /**
+     * @inheritdoc
+     */
+    public function __construct(?string $name = null)
+    {
+        $this->filesystem = new Filesystem();
+
+        parent::__construct($name);
+    }
+
+
+    /**
+     * @inheritdoc
+     */
+    protected function configure()
+    {
+        $this->setName('generate:php:cli')
+            ->setDescription('Generates proper configs')
+            ->addArgument(self::ARGUMENT_VERSION, InputArgument::REQUIRED);
+
+        parent::configure();
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @throws \InvalidArgumentException
+     * @throws FileNotFoundException
+     */
+    public function execute(InputInterface $input, OutputInterface $output)
+    {
+        $version = $input->getArgument(self::ARGUMENT_VERSION);
+
+        if (!\in_array($version, self::SUPPORTED_VERSIONS, true)) {
+            throw new \InvalidArgumentException('Not supported version');
+        }
+
+        foreach (self::EDITIONS as $edition) {
+            $this->copyData($version, $edition);
+        }
+
+        $output->writeln('<info>Done</info>');
+    }
+
+    /**
+     * @param string $version
+     * @param string $edition
+     * @throws \Illuminate\Contracts\Filesystem\FileNotFoundException
+     */
+    private function copyData(string $version, string $edition)
+    {
+        $destination = BP . '/' . $version . '-' . $edition;
+        $dataDir = DATA . '/php-' . $edition;
+
+        $this->filesystem->deleteDirectory($destination);
+        $this->filesystem->makeDirectory($destination);
+        $this->filesystem->copyDirectory($dataDir, $destination);
+
+        $dockerfile = $destination . '/Dockerfile';
+        $content = str_replace(
+            '{%version%}',
+            $version,
+            $this->filesystem->get($dockerfile)
+        );
+
+        $this->filesystem->put($dockerfile, $content);
+    }
+}
