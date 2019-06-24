d99feab49a36ba96d6766f5fb8e6c6142ed679f5
Yevhen Miroshnychenko <ymiroshnychenko@magento.com>
2019-06-24 16:09:01 -0500

    MAGECLOUD-3832: [Cloud Docker] Update libsodium library

--- src/Command/Generate/Php.php
+++ src/Command/Generate/Php.php
@@ -24,11 +24,13 @@ class Php extends Command
     private const EDITIONS = [self::EDITION_CLI, self::EDITION_FPM];
     private const ARGUMENT_VERSION = 'version';
     private const DEFAULT_PACKAGES_PHP_FPM = [
+        'apt-utils',
         'sendmail-bin',
         'sendmail',
         'sudo'
     ];
     private const DEFAULT_PACKAGES_PHP_CLI = [
+        'apt-utils',
         'sendmail-bin',
         'sendmail',
         'sudo',
@@ -42,13 +44,38 @@ class Php extends Command
         'vim',
     ];
 
+    private const PHP_EXTENSIONS_ENABLED_BY_DEFAULT = [
+        'bcmath',
+        'bz2',
+        'calendar',
+        'exif',
+        'gd',
+        'gettext',
+        'intl',
+        'mysqli',
+        'mcrypt',
+        'pcntl',
+        'pdo_mysql',
+        'soap',
+        'sockets',
+        'sysvmsg',
+        'sysvsem',
+        'sysvshm',
+        'redis',
+        'opcache',
+        'xsl',
+        'zip',
+    ];
+
     const DOCKERFILE = 'Dockerfile';
     const EXTENSION_OS_DEPENDENCIES = 'extension_os_dependencies';
     const EXTENSION_PACKAGE_NAME = 'extension_package_name';
     const EXTENSION_TYPE = 'extension_type';
     const EXTENSION_TYPE_PECL = 'extension_type_pecl';
     const EXTENSION_TYPE_CORE = 'extension_type_core';
+    const EXTENSION_TYPE_INSTALLATION_SCRIPT = 'extension_type_installation_script';
     const EXTENSION_CONFIGURE_OPTIONS = 'extension_configure_options';
+    const EXTENSION_INSTALLATION_SCRIPT = 'extension_installation_script';
 
     /**
      * @var Filesystem
@@ -92,7 +119,7 @@ class Php extends Command
     /**
      * {@inheritdoc}
      *
-     * @throws \InvalidArgumentException
+     * @throws \Illuminate\Contracts\Filesystem\FileNotFoundException
      */
     public function execute(InputInterface $input, OutputInterface $output)
     {
@@ -137,78 +164,90 @@ class Php extends Command
      * @param string $phpVersion
      * @param string $edition
      * @return string
-     * @throws \Illuminate\Contracts\Filesystem\FileNotFoundException
+     * @throws \Illuminate\Contracts\Filesystem\FileNotFoundException|\RuntimeException
      */
     private function buildDockerfile(string $dockerfile, string $phpVersion, string $edition): string
     {
         $phpConstraintObject = new Constraint('==', $this->versionParser->normalize($phpVersion));
-        $extensionConfig = $this->filesystem->getRequire(DATA . '/php-extensions.php');
+        $phpExtConfigs = $this->filesystem->getRequire(DATA . '/php-extensions.php');
 
-        $dockerPhpExtInstall = [];
-        $phpPeclExtensions = [];
-        $extOsDependencies = [];
-        $dockerPhpExtConfigure = [];
-        $dockerPhpExtEnable = [];
+        $packages = self::EDITION_CLI == $edition ? self::DEFAULT_PACKAGES_PHP_CLI : self::DEFAULT_PACKAGES_PHP_FPM;
+        $phpExtCore = [];
+        $phpExtCoreConfigOptions = [];
+        $phpExtList = [];
+        $phpExtPecl = [];
+        $phpExtInstScripts = [];
+        $phpExtEnabledDefault = [];
 
-        foreach ($extensionConfig as $phpExtName => $phpExtConfig) {
-            if (empty($phpExtName)) {
-                throw new \RuntimeException('Extension name can\'t be empty');
+        foreach ($phpExtConfigs as $phpExtName => $phpExtConfig) {
+            if (!is_string($phpExtName)) {
+                throw new \RuntimeException('Extension name not set');
             }
             foreach ($phpExtConfig as $phpExtConstraint => $phpExtInstallConfig) {
                 $phpExtConstraintObject = $this->versionParser->parseConstraints($phpExtConstraint);
-                if ($phpConstraintObject->matches($phpExtConstraintObject)) {
-                    switch ($phpExtInstallConfig[self::EXTENSION_TYPE]) {
-                        case self::EXTENSION_TYPE_CORE:
-                            $dockerPhpExtInstall[] = $phpExtInstallConfig[self::EXTENSION_PACKAGE_NAME] ?? $phpExtName;
-                            break;
-                        case self::EXTENSION_TYPE_PECL:
-                            $phpPeclExtensions[] = $phpExtInstallConfig[self::EXTENSION_PACKAGE_NAME] ?? $phpExtName;
-                            break;
-                        default:
-                            throw new \RuntimeException(sprintf(
-                                "PHP extension %s. The type %s not supported",
-                                $phpExtName,
-                                $phpExtInstallConfig[self::EXTENSION_TYPE]
-                            ));
-                    }
-                    if (isset($phpExtInstallConfig[self::EXTENSION_OS_DEPENDENCIES])) {
-                        $extOsDependencies = array_merge(
-                            $extOsDependencies,
-                            $phpExtInstallConfig[self::EXTENSION_OS_DEPENDENCIES]
-                        );
-                    }
-                    if (isset($phpExtInstallConfig[self::EXTENSION_CONFIGURE_OPTIONS])) {
-                        $dockerPhpExtConfigure[] = sprintf(
-                            "RUN docker-php-ext-configure \\\n  %s %s",
-                            $phpExtName,
-                            implode(' ', $phpExtInstallConfig[self::EXTENSION_CONFIGURE_OPTIONS])
-                        );
-                    }
-
-                    $dockerPhpExtEnable[] = $phpExtName;
+                if (!$phpConstraintObject->matches($phpExtConstraintObject)) {
+                    continue;
                 }
+                $phpExtType = $phpExtInstallConfig[self::EXTENSION_TYPE];
+                switch ($phpExtType) {
+                    case self::EXTENSION_TYPE_CORE:
+                        $phpExtCore[] = $phpExtInstallConfig[self::EXTENSION_PACKAGE_NAME] ?? $phpExtName;
+                        if (isset($phpExtInstallConfig[self::EXTENSION_CONFIGURE_OPTIONS])) {
+                            $phpExtCoreConfigOptions[] = sprintf(
+                                "RUN docker-php-ext-configure \\\n  %s %s",
+                                $phpExtName,
+                                implode(' ', $phpExtInstallConfig[self::EXTENSION_CONFIGURE_OPTIONS])
+                            );
+                        }
+                        break;
+                    case self::EXTENSION_TYPE_PECL:
+                        $phpExtPecl[] = $phpExtInstallConfig[self::EXTENSION_PACKAGE_NAME] ?? $phpExtName;
+                        break;
+                    case self::EXTENSION_TYPE_INSTALLATION_SCRIPT:
+                        $phpExtInstScripts[] = implode(" \\\n", array_map(function (string $command) {
+                            return strpos($command, 'RUN') === false ? '  && ' . $command : $command;
+                        }, explode("\n", 'RUN ' . $phpExtInstallConfig[self::EXTENSION_INSTALLATION_SCRIPT])));
+                        break;
+                    default:
+                        throw new \RuntimeException(sprintf(
+                            "PHP extension %s. The type %s not supported",
+                            $phpExtName,
+                            $phpExtType
+                        ));
+                }
+                if (
+                    isset($phpExtInstallConfig[self::EXTENSION_OS_DEPENDENCIES])
+                    && $phpExtType != self::EXTENSION_TYPE_INSTALLATION_SCRIPT
+                ) {
+                    $packages = array_merge($packages, $phpExtInstallConfig[self::EXTENSION_OS_DEPENDENCIES]);
+                }
+                if (in_array($phpExtName, self::PHP_EXTENSIONS_ENABLED_BY_DEFAULT)) {
+                    $phpExtEnabledDefault[] = $phpExtName;
+                }
+                $phpExtList[] = $phpExtName;
             }
         }
-
-        $packages = array_merge(
-            self::EDITION_CLI == $edition ? self::DEFAULT_PACKAGES_PHP_CLI : self::DEFAULT_PACKAGES_PHP_FPM,
-            $extOsDependencies
-        );
 
         return strtr(
             $this->filesystem->get($dockerfile),
             [
                 '{%version%}' => $phpVersion,
                 '{%packages%}' => implode(" \\\n  ", array_unique($packages)),
-                '{%docker-php-ext-configure%}' => implode(PHP_EOL, $dockerPhpExtConfigure),
-                '{%docker-php-ext-install%}' => !empty($dockerPhpExtInstall)
-                    ? "RUN docker-php-ext-install -j$(nproc) \\\n  " . implode(" \\\n  ", $dockerPhpExtInstall)
+                '{%docker-php-ext-configure%}' => implode(PHP_EOL, $phpExtCoreConfigOptions),
+                '{%docker-php-ext-install%}' => !empty($phpExtCore)
+                    ? "RUN docker-php-ext-install -j$(nproc) \\\n  " . implode(" \\\n  ", $phpExtCore)
                     : '',
-                '{%php-pecl-extensions%}' => !empty($phpPeclExtensions)
-                    ? "RUN pecl install -o -f \\\n  " . implode(" \\\n  ", $phpPeclExtensions)
+                '{%php-pecl-extensions%}' => !empty($phpExtPecl)
+                    ? "RUN pecl install -o -f \\\n  " . implode(" \\\n  ", $phpExtPecl)
                     : '',
-                '{%docker-php-ext-enable%}' => !empty($dockerPhpExtEnable)
-                    ? "RUN docker-php-ext-enable \\\n  " . implode(" \\\n  ", $dockerPhpExtEnable)
+                '{%docker-php-ext-enable%}' => !empty($phpExtList)
+                    ? "RUN docker-php-ext-enable \\\n  " . implode(" \\\n  ", $phpExtList)
+                    : '',
+                '{%installation_scripts%}' => !empty($phpExtInstScripts)
+                    ? implode(PHP_EOL, $phpExtInstScripts)
+                    : '',
+                '{%env_php_extensions%}' => !(empty($phpExtEnabledDefault))
+                    ? 'ENV PHP_EXTENSIONS ' . implode(' ', $phpExtEnabledDefault)
                     : '',
             ]
         );
