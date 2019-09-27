894ab1f1f37e13251b9cfbeb5e2602e166946db1
Oleh Posyniak <oposyniak@magento.com>
2019-09-27 14:35:51 -0500

    MAGECLOUD-4292: FT Take Longer than Before Because of composer/cache Directory has Read-only Permissions

--- images/nginx/1.9/docker-entrypoint.sh
+++ images/nginx/1.9/docker-entrypoint.sh
@@ -2,31 +2,6 @@
 
 [ "$DEBUG" = "true" ] && set -x
 
-# If asked, we'll ensure that the www is set to the same uid/gid as the
-# mounted volume.  This works around permission issues with virtualbox shared
-# folders.
-if [[ "$UPDATE_UID_GID" = "true" ]]; then
-    echo "Updating www uid and gid"
-
-    DOCKER_UID=`stat -c "%u" $MAGENTO_ROOT/.composer`
-    DOCKER_GID=`stat -c "%g" $MAGENTO_ROOT/.composer`
-
-    INCUMBENT_USER=`getent passwd $DOCKER_UID | cut -d: -f1`
-    INCUMBENT_GROUP=`getent group $DOCKER_GID | cut -d: -f1`
-
-    echo "Docker: uid = $DOCKER_UID, gid = $DOCKER_GID"
-    echo "Incumbent: user = $INCUMBENT_USER, group = $INCUMBENT_GROUP"
-
-    # Once we've established the ids and incumbent ids then we need to free them
-    # up (if necessary) and then make the change to www.
-
-    [ ! -z "${INCUMBENT_USER}" ] && usermod -u 99$DOCKER_UID $INCUMBENT_USER
-    usermod -u $DOCKER_UID www
-
-    [ ! -z "${INCUMBENT_GROUP}" ] && groupmod -g 99$DOCKER_GID $INCUMBENT_GROUP
-    groupmod -g $DOCKER_GID www
-fi
-
 VHOST_FILE="/etc/nginx/conf.d/default.conf"
 
 [ ! -z "${FPM_HOST}" ] && sed -i "s/!FPM_HOST!/${FPM_HOST}/" $VHOST_FILE
