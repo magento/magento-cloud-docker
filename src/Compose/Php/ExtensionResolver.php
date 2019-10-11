9aea5cb8e73d1cce619094421babc52f541d1f2a
Bohdan Korablov <korablov@adobe.com>
2019-10-11 09:24:12 -0500

    MAGECLOUD-4354: [GitHub] Add ionCube Support #70

--- src/Compose/Php/ExtensionResolver.php
+++ src/Compose/Php/ExtensionResolver.php
@@ -389,10 +389,9 @@ BASH
 cd /tmp
 curl -O https://downloads.ioncube.com/loader_downloads/ioncube_loaders_lin_x86-64.tar.gz
 tar zxvf ioncube_loaders_lin_x86-64.tar.gz
-export PHP_CONFD=$(php-config --configure-options|sed 's/.*\with-config-file-scan-dir\=\(\S*\).*/\\1/g')
 export PHP_VERSION=$(php -r "echo PHP_MAJOR_VERSION.'.'.PHP_MINOR_VERSION;")
 export PHP_EXT_DIR=$(php-config --extension-dir)
-cp "ioncube/ioncube_loader_lin_\${PHP_VERSION}.so" "\${PHP_EXT_DIR}/ioncube.so"
+cp "./ioncube/ioncube_loader_lin_\${PHP_VERSION}.so" "\${PHP_EXT_DIR}/ioncube.so"
 rm -rf ./ioncube
 rm ioncube_loaders_lin_x86-64.tar.gz
 BASH
