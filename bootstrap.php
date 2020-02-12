0e38b3d9d83a8cecf33bdd8adb127258b69cb9aa
Oleh Posyniak <oposyniak@magento.com>
2020-02-12 10:49:10 -0600

    MAGECLOUD-5185: [GitHub] Platform-agnostic Docker builder #102

--- bootstrap.php
+++ bootstrap.php
@@ -3,9 +3,6 @@
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
-error_reporting(E_ALL);
-date_default_timezone_set('UTC');
-
 require __DIR__ . '/autoload.php';
 
 use Magento\CloudDocker\App\Container;
