1b0e1bf188b8cb99e8b1178f0b41785f9a550aff
Oleksandr Shmyheliuk <oshmyheliuk@magento.com>
2020-03-04 16:33:29 -0600

    MAGECLOUD-4168: Provide an Option to Specify Custom URL for Magento

--- src/Test/Functional/Acceptance/AcceptanceCest.php
+++ src/Test/Functional/Acceptance/AcceptanceCest.php
@@ -54,12 +54,15 @@ class AcceptanceCest
      */
     public function testCustomHost(\CliTester $I): void
     {
-        $I->updateBaseUrl('magento2.test:8080');
-        $I->runEceDockerCommand('build:compose --mode=production --host=magento2.test --port=8080');
+        $I->updateBaseUrl('http://magento2.test:8080/');
+        $I->assertTrue(
+            $I->runEceDockerCommand('build:compose --mode=production --host=magento2.test --port=8080'),
+            'Command build:compose failed'
+        );
         $I->startEnvironment();
-        $I->runDockerComposeCommand('run build cloud-build');
-        $I->runDockerComposeCommand('run deploy cloud-deploy');
-        $I->runDockerComposeCommand('run deploy cloud-post-deploy');
+        $I->assertTrue($I->runDockerComposeCommand('run build cloud-build'), 'Build phase failed');
+        $I->assertTrue($I->runDockerComposeCommand('run deploy cloud-deploy'), 'Deploy phase failed');
+        $I->assertTrue($I->runDockerComposeCommand('run deploy cloud-post-deploy'), 'Post deploy phase failed');
         $I->amOnPage('/');
         $I->see('Home page');
         $I->see('CMS homepage content goes here.');
