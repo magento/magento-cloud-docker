da98f21e02dfc3d1aab5938673477f10eb839815
Bohdan Korablov <korablov@adobe.com>
2020-04-07 16:01:43 -0500

    MCLOUD-5603: [Investigation] Build images on travis for testing

---
+++ tests/travis/images.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Copyright © Magento, Inc. All rights reserved.
+# See COPYING.txt for license details.
+
+set -e
+trap '>&2 echo Error: Command \`$BASH_COMMAND\` on line $LINENO failed with exit code $?' ERR
+
+function build_push_image() {
+  image_name="$1:$2"
+  docker build -t "$image_name" "$3"
+  docker push "$image_name"
+}
+
+build_push_image "cloudft/tls" "$TRAVIS_BUILD_NUMBER" "./images/tls"
+
+for service_name in elasticsearch nginx redis varnish php
+do
+   for service_version in $(ls -1 "./images/$service_name")
+   do
+    if [ $service_version == "cli" ] || [ $service_version == "fpm" ]; then continue; fi;
+    build_push_image "cloudft/$service_name" "$service_version-$TRAVIS_BUILD_NUMBER" "./images/$service_name/$service_version"
+   done
+done
