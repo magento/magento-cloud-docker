a24ca3ac72fa77557c4fef788dd5e303d7104bbf
ashvini22326 <pru22326@adobe.com>
2025-07-14 18:00:01 +0530

    updated default test

--- src/Test/Functional/Acceptance/Valkey83Cest.php
+++ src/Test/Functional/Acceptance/Valkey83Cest.php
@@ -8,12 +8,12 @@ declare(strict_types=1);
 namespace Magento\CloudDocker\Test\Functional\Acceptance;
 
 /**
- * @group php84
+ * @group php83
  */
-class Valkey84Cest extends ValkeyCest
+class Valkey83Cest extends ValkeyCest
 {
     /**
      * Template version for testing
      */
-    protected const TEMPLATE_VERSION = '2.4.8';
+    protected const TEMPLATE_VERSION = '2.4.7';
 }
