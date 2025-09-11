2a1adf13ec272c02e049af19b755fdf6f0a20d11
shruthi malode <del21738@adobe.com>
2025-09-11 19:02:04 +0530

    MCLOUD-14015:Adding logs

--- tests/functional/Robo/Tasks/CopyFromDocker.php
+++ tests/functional/Robo/Tasks/CopyFromDocker.php
@@ -79,12 +79,21 @@ class CopyFromDocker extends BaseTask implements CommandInterface
      */
     public function getCommand(): string
     {
-        return sprintf(
-            'docker cp "$(docker-compose ps -q %s)":%s %s',
+    // Log individual parts
+        error_log("Container: " . $this->container);
+        error_log("Source path: " . $this->source);
+        error_log("Destination path: " . $this->destination);
+
+        // Compose the command
+        $command = sprintf(
+            'docker-compose cp %s:%s %s',
             $this->container,
             $this->source,
             $this->destination
         );
+
+        // Log the final command
+        error_log("Final docker command: " . $command);
     }
 
     /**
