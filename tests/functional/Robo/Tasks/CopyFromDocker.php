62df8002e27ab9570b93d080e6b5c25b4915aaf3
shruthi malode <del21738@adobe.com>
2025-09-15 19:35:56 +0530

    MCLOUD-14015:Debug for docker cp

--- tests/functional/Robo/Tasks/CopyFromDocker.php
+++ tests/functional/Robo/Tasks/CopyFromDocker.php
@@ -85,19 +85,12 @@ class CopyFromDocker extends BaseTask implements CommandInterface
     error_log('Source: ' . $this->source);
     error_log('Destination: ' . $this->destination);
 
-    // return sprintf(
-    //     'docker-compose cp %s:%s %s',
-    //     $this->container,
-    //     $this->source,
-    //     $this->destination
-    // );
     return sprintf(
-        'docker-compose cp %s %s:%s',
+        'docker-compose cp %s:%s %s',
         $this->container,
         $this->source,
         $this->destination
     );
-
     }
 
     /**
