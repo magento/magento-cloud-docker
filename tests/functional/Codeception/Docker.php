059273b8d3b68b877b8324b4bd6086f1825cf9cf
shruthi malode <del21738@adobe.com>
2025-09-23 18:37:21 +0530

    MCLOUD-14015:small change

--- tests/functional/Codeception/Docker.php
+++ tests/functional/Codeception/Docker.php
@@ -108,7 +108,7 @@ class Docker extends BaseModule
     public function resetFilesOwner(): bool
     {
         return $this->runDockerComposeCommand(
-            'run --user root build bash -c "uid=$(stat -c %u . 2>/dev/null || stat -f %u .); gid=$(stat -c %g . 2>/dev/null || stat -f %g .); chown -R $uid:$gid . /composer/cache"'
+            'run build bash -c "chown -R $(id -u):$(id -g) . /composer/cache"'
         ); 
     }
 
