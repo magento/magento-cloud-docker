34564c034eb4b8e9e553f0249e7dc3019fc8d001
shruthi malode <del21738@adobe.com>
2025-09-24 12:25:47 +0530

    MCLOUD-14015: Recheck the cp command

--- tests/functional/Robo/Tasks/CopyToDocker.php
+++ tests/functional/Robo/Tasks/CopyToDocker.php
@@ -83,13 +83,18 @@ class CopyToDocker extends BaseTask implements CommandInterface
     error_log('Container for cp: ' . $this->container);
     error_log('Source: ' . $this->source);
     error_log('Destination: ' . $this->destination);
-
+    $check_ps_command = sprintf('docker-compose ps -q %s', $this->container);
+    error_log('ps command: ' . $check_ps_command);
     return sprintf(
-        'docker-compose cp %s %s:%s',
-        $this->container,
-        $this->source,
-        $this->destination
-    );
+    '(docker cp %s:%s %s) || (docker-compose cp %s:%s %s)',
+    $check_ps_command,
+    $this->source,
+    $this->destination,
+    $this->container,
+    $this->source,
+    $this->destination
+);
+
 }
 
     /**
